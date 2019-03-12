bfcb3af450bc4a570cac3f7ba1d186fde420fdcb
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-12 21:18:48 +0300

    refactor calc game

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -1,26 +1,28 @@
 <?php
 
-namespace BrainGames\Calc;
+namespace BrainGames\games\Calc;
 
 use function BrainGames\Cli\run;
 
 const TASK = "What is the result of the expression?";
-const OPERATION = ['*', '+', '-'];
 
-function getQuestion()
-{
-    $firstOperand = rand(0, 100);
-    $secondOperand = rand(0, 100);
-    $operation = OPERATION[array_rand(OPERATION)];
-
-    return "$firstOperand $operation $secondOperand";
+function getOperation() {
+    return [
+        ['op' => '*', 'func' => function ($a, $b) { return $a * $b; }],
+        ['op' => '+', 'func' => function ($a, $b) { return $a + $b; }],
+        ['op' => '-', 'func' => function ($a, $b) { return $a - $b; }]
+    ];
 }
 
 function start()
 {
     run(TASK, function () {
-        $question = getQuestion();
-        $answer = (string) eval("return $question;");
+        $a = rand(0, 100);
+        $b = rand(0, 100);
+        $operation = getOperation()[rand(0, 2)];
+
+        $question = "$a {$operation['op']} $b";
+        $answer = (string) $operation['func']($a, $b);
 
         return [$question, $answer];
     });
