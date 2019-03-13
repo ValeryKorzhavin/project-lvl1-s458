000bea6f5c04871e42180844083d03e1079f8ad6
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-13 11:40:32 +0300

    refactor calc operations

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -6,36 +6,32 @@ use function BrainGames\Main\run;
 
 const TASK = "What is the result of the expression?";
 
-function getOperation()
-{
-    return [
-        [
-            'op' => '*', 'func' => function ($a, $b) {
-                return $a * $b;
-            }
-        ],
-        [
-            'op' => '+', 'func' => function ($a, $b) {
-                return $a + $b;
-            }
-        ],
-        [
-            'op' => '-', 'func' => function ($a, $b) {
-                return $a - $b;
-            }
-        ]
-    ];
-}
-
 function start()
 {
     run(TASK, function () {
         $a = rand(0, 100);
         $b = rand(0, 100);
-        $operation = getOperation()[rand(0, 2)];
+        $operations = [
+            [
+                'sign' => '*', 'operation' => function ($a, $b) {
+                    return $a * $b;
+                }
+            ],
+            [
+                'sign' => '+', 'operation' => function ($a, $b) {
+                    return $a + $b;
+                }
+            ],
+            [
+                'sign' => '-', 'operation' => function ($a, $b) {
+                    return $a - $b;
+                }
+            ]
+        ];
+        $operation = $operations[rand(0, 2)];
 
-        $question = "$a {$operation['op']} $b";
-        $rightAnswer = (string) $operation['func']($a, $b);
+        $question = "$a {$operation['sign']} $b";
+        $rightAnswer = (string) $operation['operation']($a, $b);
 
         return [$question, $rightAnswer];
     });
