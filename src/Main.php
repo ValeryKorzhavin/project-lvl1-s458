33b41333c8ad31440db28d80cf9800a25433878d
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-13 11:55:07 +0300

    refactor Main

--- src/Main.php
+++ src/Main.php
@@ -7,17 +7,13 @@ use function cli\prompt;
 
 const ROUNDS_COUNT = 3;
 
-function run(string $task = '', callable $generator = null): void
+function run(string $task, callable $generator): void
 {
     line('Welcome to the Brain Games!');
-    !$task ?: line($task);
+    line($task);
     line();
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    
-    if (!$task || !$generator) {
-        return;
-    }
     line();
 
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
